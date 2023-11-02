bff5a3d70484dbb2fbc1b4d8c0f98ff4b0f03fc1
Jhenja Fedko <fedkozhenja@gmail.com>
2023-11-03 02:12:43 +0400

    feat: add content to about page

--- src/application/Router/Router.tsx
+++ src/application/Router/Router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { CardsPage } from '../../components/pages/CardsPage/CardsPage';
-import { AboutPage } from '../../components/pages/About/AboutPage';
+import { AboutPage } from '../../components/pages/AboutPage/AboutPage';
 import { Layout } from '../Layout/Layout';
 import { Path } from '../path';
 
---
+++ src/components/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,25 @@
+import './AboutPage.css';
+
+export const AboutPage = () => {
+    return (
+        <div className="about-wrapper">
+            <div className="about-info">
+                <h3>Welcome to our application!</h3>
+                <p>
+                    We are pleased to welcome you to the wonderful world of Pokemon. Our app is designed to help you
+                    find all the information you need about your favorite Pokemon.
+                </p>
+                <p>
+                    With our application you can easily find information about Pokemon, their types, characteristics,
+                    abilities and much more.
+                </p>
+                <p>
+                    No matter what level of experience you have in this world, our app will help you dive deeper into
+                    the Pokemon universe. Thank you for choosing us, and welcome to an exciting adventure of finding and
+                    exploring the world of Pokemon!
+                </p>
+            </div>
+            <div className="about-img" />
+        </div>
+    );
+};
