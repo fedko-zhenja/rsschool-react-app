c8143513ed5e5395168bc504ce29a52de090c2f7
@fedko-zhenja <fedkozhenja@gmail.com>
2023-11-21 21:31:46 +0400

    refactor: update AdditionalCardsInfo test

--- src/test/AdditionalCardsInfo.test.tsx
+++ src/test/AdditionalCardsInfo.test.tsx
@@ -1,69 +1,79 @@
-// import React from 'react';
-// import { render, screen, waitFor, fireEvent } from '@testing-library/react';
-// import { MemoryRouter, Routes, Route } from 'react-router-dom';
-// import { AdditionalCardsInfo } from '../pages/CardsPage/components/AdditionalCardsInfo/AdditionalCardsInfo';
-// import * as PokemonApiModule from '../api/PokemonApi';
-// import { mockCardData } from './mockData';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
+import { AdditionalCardsInfo } from '../pages/CardsPage/components/AdditionalCardsInfo/AdditionalCardsInfo';
+import { mockCardData } from './mockData';
+import * as api from '../api/PokemonApi';
 
-// const mockGetCardDataById = jest.spyOn(PokemonApiModule, 'getCardDataById');
-// jest.mock('../api/PokemonApi');
+const mockUseGetCardByIdQuery = api.useGetCardByIdQuery as jest.MockedFunction<typeof api.useGetCardByIdQuery>;
 
-// describe.skip('AdditionalCardsInfo', () => {
-//     test('should display a loading indicator when receiving data', async () => {
-//         mockGetCardDataById.mockResolvedValueOnce(mockCardData);
+jest.mock('../api/PokemonApi', () => ({
+    ...jest.requireActual('../api/PokemonApi'),
+    useGetCardByIdQuery: jest.fn(),
+}));
 
-//         render(
-//             <MemoryRouter initialEntries={['/cards/1']}>
-//                 <Routes>
-//                     <Route path="/cards/:index" element={<AdditionalCardsInfo />} />
-//                 </Routes>
-//             </MemoryRouter>
-//         );
+describe('AdditionalCardsInfo', () => {
+    test('should display a loading indicator when receiving data', async () => {
+        mockUseGetCardByIdQuery.mockReturnValue({
+            data: mockCardData,
+            isFetching: true,
+            refetch: jest.fn(),
+        });
 
-//         expect(screen.getByText('Loading...')).toBeInTheDocument();
+        render(
+            <MemoryRouter initialEntries={['/cards/1']}>
+                <Routes>
+                    <Route path="/cards/:index" element={<AdditionalCardsInfo />} />
+                </Routes>
+            </MemoryRouter>
+        );
 
-//         await waitFor(() => {
-//             expect(screen.queryByText('Loading...')).toBeNull();
-//             expect(screen.getByText('Name:')).toBeInTheDocument();
-//         });
-//     });
+        expect(screen.getByText('Loading...')).toBeInTheDocument();
+    });
 
-//     test('should display card details correctly', async () => {
-//         mockGetCardDataById.mockResolvedValueOnce(mockCardData);
+    test('should display card details correctly', async () => {
+        mockUseGetCardByIdQuery.mockReturnValue({
+            data: mockCardData,
+            isFetching: false,
+            refetch: jest.fn(),
+        });
 
-//         render(
-//             <MemoryRouter initialEntries={['/cards/1']}>
-//                 <Routes>
-//                     <Route path="/cards/:index" element={<AdditionalCardsInfo />} />
-//                 </Routes>
-//             </MemoryRouter>
-//         );
+        render(
+            <MemoryRouter initialEntries={['/cards/1']}>
+                <Routes>
+                    <Route path="/cards/:index" element={<AdditionalCardsInfo />} />
+                </Routes>
+            </MemoryRouter>
+        );
 
-//         await waitFor(() => {
-//             expect(screen.getByText('Name:')).toBeInTheDocument();
-//             expect(screen.getByText('Test Card')).toBeInTheDocument();
-//             expect(screen.getByText('Health power:')).toBeInTheDocument();
-//             expect(screen.getByText('100')).toBeInTheDocument();
-//         });
-//     });
+        await waitFor(() => {
+            expect(screen.getByText('Name:')).toBeInTheDocument();
+            expect(screen.getByText('Test Card')).toBeInTheDocument();
+            expect(screen.getByText('Health power:')).toBeInTheDocument();
+            expect(screen.getByText('100')).toBeInTheDocument();
+        });
+    });
 
-//     test('should hide the component when the close button is clicked', async () => {
-//         mockGetCardDataById.mockResolvedValueOnce(mockCardData);
+    test('should hide the component when the close button is clicked', async () => {
+        mockUseGetCardByIdQuery.mockReturnValue({
+            data: mockCardData,
+            isFetching: false,
+            refetch: jest.fn(),
+        });
 
-//         render(
-//             <MemoryRouter initialEntries={['/cards/1']}>
-//                 <Routes>
-//                     <Route path="/cards/:index" element={<AdditionalCardsInfo />} />
-//                 </Routes>
-//             </MemoryRouter>
-//         );
+        render(
+            <MemoryRouter initialEntries={['/cards/1']}>
+                <Routes>
+                    <Route path="/cards/:index" element={<AdditionalCardsInfo />} />
+                </Routes>
+            </MemoryRouter>
+        );
 
-//         await waitFor(() => {
-//             expect(screen.getByText('Name:')).toBeInTheDocument();
-//         });
+        await waitFor(() => {
+            expect(screen.getByText('Name:')).toBeInTheDocument();
+        });
 
-//         fireEvent.click(screen.getByText('X'));
+        fireEvent.click(screen.getByText('X'));
 
-//         expect(screen.queryByText('Name:')).toBeNull();
-//     });
-// });
+        expect(screen.queryByText('Name:')).toBeNull();
+    });
+});
