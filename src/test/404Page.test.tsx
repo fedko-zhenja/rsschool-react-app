c7b1432c4bee2d14ada2bcca14e5c9398ebe1e59
@fedko-zhenja <fedkozhenja@gmail.com>
2023-11-29 19:30:58 +0400

    fix: update 404Page.test

--- src/test/404Page.test.tsx
+++ src/test/404Page.test.tsx
@@ -1,16 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import NotFound from '../pages/404';
 
 describe('404Page', () => {
     test('should render the NotFound component on the wrong route', () => {
-        render(
-            <MemoryRouter initialEntries={['/']}>
-                <Routes>
-                    <Route path="*" element={<NotFound />}></Route>
-                </Routes>
-            </MemoryRouter>
-        );
+        render(<NotFound />);
 
         const notFoundElement = screen.getByText(/Not Found.../i);
         expect(notFoundElement).toBeInTheDocument();
